c62af6ff191d92077705ef4efb935309b6c8e2ba
RINSHIDKT <rinshidkt6@gmail.com>
2023-11-07 13:44:02 +0530

    file added

--- controller.js
+++ controller.js
@@ -1,5 +1,7 @@
 import schema from "./user.model.js";
 import bcrypt from "bcrypt";
+import pkg from "jsonwebtoken";
+const {sign}=pkg;
 
 
 export function addUser(req,res)
@@ -23,7 +25,34 @@ export function addUser(req,res)
     } catch (error) {
         console.log(error);
     }
-  
-    
+
+
+}
+export async function login(req,res)
+{
+    console.log(req.body);
+    const{user,password}=req.body;
+    const usr=await schema.findOne({user})
+    console.log(usr);
+    if (usr===null) return res.status(404).send("username or password doesnot exist");
+    const success=await bcrypt.compare(password,usr.password)
+    console.log(success);
+    if(success!==true) return res.status(404).send("username or password doesnot exist");
+    const token=await sign({user},process.env.JWT_KEY,{expiresIn:"24h"})
+    console.log(token);
+    res.status(200).send({msg:"successfully login",token})
+    res.end();
+}
+export async function home(req,res)
+{
+    try{
+        console.log(req.user);
+        const username=req.user.user
+        console.log(username);
+        res.status(200).send({msg:`hello ${username}`})
+        
+    } catch (error){
+        res.status(404).send(error)
+    }
 }
 
--- router.js
+++ router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import Auth from "./middleware/Auth.js";
 import * as controller from "./controller.js";
 
 const router=Router();
 
 router.route("/adduser").post(controller.addUser);
+router.route("/login").post(controller.login);
+router.route("/home").get(Auth,controller.home);
 
 
 export default router;
